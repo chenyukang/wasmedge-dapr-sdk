7a5be8492f3039d1975566fa26c0e07d9fb35e1a
yukang <moorekang@gmail.com>
2022-02-28 20:56:06 +0800

    cleanup

--- client/abi_state.go
+++
@@ -1,7 +0,0 @@
-
-package client
-
-// todo finish the real abi
-
-//export dapr_state
-func daprState(action action, storeName string, key string, data []byte, consistency int32,metadata string) (res string, err string)
--- example/hello-world/readme.md
+++
@@ -1,4 +0,0 @@
-
-```shell
-tinygo build -o hello-world.wasm -scheduler=none -target=wasi  ./order.go
-```
